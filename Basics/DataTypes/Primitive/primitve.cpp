15a1c21abd78cb3ec41ce727d0f7eb654a741534
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-07-28 20:37:01 +0530

    Primitive Datatype

---
+++ Basics/DataTypes/Primitive/primitve.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "Size of int : " << sizeof(int) << endl;                    // 4
+	cout << "Size of char : " << sizeof(char) << endl;                  // 1
+	cout << "Size of bool : " << sizeof(bool) << endl;                  // 1
+	cout << "Size of float : " << sizeof(float) << endl;                // 4  
+	cout << "Size of double : " << sizeof(double) << endl;              // 8
+	// cout << "Size of void : " << sizeof(void) << endl;               // Value less quantity
+	cout << "Size of wide character : " << sizeof(wchar_t) << endl;     // 2
+	
+    
+	return 0;
+}
