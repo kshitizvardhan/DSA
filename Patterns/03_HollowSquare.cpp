0ad1cd398c4ee60756f0cf61612644bca85d3730
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-08-18 14:24:01 +0530

    Hollow Square Pattern

---
+++ Patterns/03_HollowSquare.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void HollowSquarePattern(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || i==n-1 || j==0 || j==n-1){
+                cout << "* ";
+            }else{
+                cout << "  ";   // Two spaces here to align the pattern
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    HollowSquarePattern(n);
+    return 0;
+}
