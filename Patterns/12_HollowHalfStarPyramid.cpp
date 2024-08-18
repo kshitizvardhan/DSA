3f78396b344a6d3f1b1f58f162db95f39eea661d
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-08-18 15:35:06 +0530

    Hollow Half Star Pyramid

---
+++ Patterns/12_HollowHalfStarPyramid.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void HollowHalfStarPyramidPattern(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            if(i==n-1 || j==0 || j==i){
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter a Number: ";
+    cin >> n;
+    HollowHalfStarPyramidPattern(n);
+    return 0;
+}
