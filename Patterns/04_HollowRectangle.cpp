096d7a882da432dede1596fe9da29d4e6d412135
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-08-18 14:24:16 +0530

    Hollow Rectangle Pattern

---
+++ Patterns/04_HollowRectangle.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void HollowRectanglePattern(int rows, int cols){
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            if(i==0 || i==rows-1 || j==0 || j==cols-1){
+                cout << "* ";
+            }else{
+                cout << "  ";   // Two spaces here to align the pattern
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int rows,cols;
+    cout << "Enter Number of rows and columns: ";
+    cin >> rows >> cols;
+    HollowRectanglePattern(rows, cols);
+    return 0;
+}
